fa320f87a2fc70b403ae750468f8e8d95e12fd56
Uriel Castañeda Sierra <urielcastanedasierra@gmail.com>
2026-02-23 23:37:17 -0500

    V4.3 incorporación de API para ping y archivo para configuración de conectores en Power Apps

--- src/middleware/apikey.js
+++ src/middleware/apikey.js
@@ -1,10 +1,24 @@
 // -----  valida validez de la APIKey suministrada
 export default function apiKey(req, res, next) {
+  
+  // ------  Se VERIFICA EL ENTORNO DE TRABAJO ACTUAL -----
+  //         Si está en DESARROLLO o PRUEBAS,  se puede  excluir las validaciones 
+  //         de RUTAS ORIGEN  y API-KEY
+
+  // Permitir preflight CORS
+  if (req.method === 'OPTIONS') return res.sendStatus(204);
+
+  // Bypass en desarrollo
+  if (process.env.NODE_ENV === 'development' || process.env.SKIP_API_KEY === 'true') {
+    return next();
+  }
+
+  // en PRODUCCIÓN  se dejan obligatorias las validaciones. 
   const EXPECTED = process.env.POWERAPPS_API_KEY || process.env.API_KEY || 'miClaveParaPowerApps';  
   const key = req.header('x-api-key');
   if (!key || key !== EXPECTED) {
     // ----- Genera Error 401,  correspondiene a Falla en validación de seguridad!
-    return res.status(401).json({ error: 'Unauthorized', message: 'Invalid or missing API key (x-api-key)'});
+    return res.status(401).json({ error: 'Acceso No Autorizado', message: 'API key Inválida o errada...'});
   }
   next();   // No genera Error. Acepta la validación y da continuidad al programa
 }
