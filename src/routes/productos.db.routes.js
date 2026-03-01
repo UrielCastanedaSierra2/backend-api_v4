b6fb5dc103096a2de2201988be566881bc303d41
Uriel Castañeda Sierra <urielcastanedasierra@gmail.com>
2026-03-01 01:14:18 -0500

    V4.4 incorporación API consulta porducto por id y ajuste en documentos swagger.yaml

--- src/routes/productos.db.routes.js
+++ src/routes/productos.db.routes.js
@@ -29,6 +29,36 @@ router.get('/', async (req, res) => {
 });
 
 
+// ─────────────────────────────────────────────────────────────
+// OBTENER POR ID: GET /api/productos-db/:id_producto
+// ─────────────────────────────────────────────────────────────
+router.get('/:id_producto', async (req, res) => {
+  const id = parseInt(req.params.id_producto, 10);
+  if (Number.isNaN(id)) {
+      return res.status(400).json({ error: 'id_producto inválido' });
+  }
+
+  try {
+    // ---- Aquí se lanzael Query con las instrucciones SQL
+    //      que permiten obtener el productos solicitado
+    const [rows] = await pool.query(
+      `SELECT nombre, foto, votacion FROM productos
+            WHERE id_producto = ?`,
+            [id]      
+    );
+
+    if (rows.length === 0) return res.status(404).json({ error: 'producto No encontrado' });
+    console.log(`✅ OK consulta producto...(${ id })`);     
+    return res.json(rows[0]);
+
+  } catch (err) {
+        console.error(err);
+        return res.status(500).json({ error: 'Error consultando producto' });
+  }
+});
+
+
+
 // ===============================
 // PUT /api/productos-db/votar/:nombre
 // Incrementa en +1 la votación
